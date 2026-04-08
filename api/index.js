5b615a7180143fc49085baedf429af0298a121c8
Kavish Joaheer <kavishjoaheer57@gmail.com>
2026-04-08 15:54:25 +0400

    Route Vercel API through single function

--- api/index.js
+++ api/index.js
@@ -1,5 +1,22 @@
+const { URL } = require("node:url");
+
 const { hasPostgresConfig } = require("../server/src/pg");
 const { createApp } = require("../server/src/app");
 const { createPostgresApp } = require("../server/src/vercel-postgres-app");
 
-module.exports = hasPostgresConfig ? createPostgresApp() : createApp();
+const app = hasPostgresConfig ? createPostgresApp() : createApp();
+
+module.exports = (req, res) => {
+  const requestUrl = new URL(req.url, "http://localhost");
+  const routeParam = requestUrl.searchParams.get("route");
+
+  if (routeParam !== null) {
+    requestUrl.searchParams.delete("route");
+    const normalizedRoute = routeParam ? `/api/${routeParam}` : "/api";
+    const search = requestUrl.searchParams.toString();
+
+    req.url = `${normalizedRoute}${search ? `?${search}` : ""}`;
+  }
+
+  return app(req, res);
+};
